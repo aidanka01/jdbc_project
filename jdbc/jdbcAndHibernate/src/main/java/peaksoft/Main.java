6f0b8decf01964a3fa87b328d401d7912c50a4bc
Aidana <108028245+aidanka01@users.noreply.github.com>
2022-09-27 18:14:24 +0300

    1st commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,38 @@
 package peaksoft;
 
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserDaoJdbcImpl userDaoJdbc = new UserDaoJdbcImpl();
+
+        //CREATE TABLE
+        userDaoJdbc.createUsersTable();
+
+        // ADD USERS
+        userDaoJdbc.saveUser("Azema", "Kadyrova", (byte) 26);
+        userDaoJdbc.saveUser( "Aidana", "Kadyrova", (byte) 23);
+        userDaoJdbc.saveUser( "Tariel", "Kadyrov", (byte) 21);
+        userDaoJdbc.saveUser( "Avtandil", "Kadyrov", (byte) 19);
+
+        // CLEAN users TABLE
+        userDaoJdbc.cleanUsersTable();
+
+        // REMOVE users BY ID
+        userDaoJdbc.removeUserById(1);
+
+        // GET ALL users
+        userDaoJdbc.getAllUsers();
+        List<User> userList = userDaoJdbc.getAllUsers();
+        System.out.println(userList);
+
+        // DROP TABLE users
+        userDaoJdbc.dropUsersTable();
     }
 }
+
+
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,23 @@
 package peaksoft.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String username = "postgres";
+    private static final String password = "123456";
+
+    public static Connection connect(){
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to db successfully");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
